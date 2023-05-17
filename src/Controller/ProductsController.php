210ec7912d8446f2220da1710a42bed4d3617df6
Chavocito <fiifiowusu@aol.com>
2023-05-17 00:47:34 +0000

    w.i.p: use db data for promotion query

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -4,8 +4,11 @@ namespace App\Controller;
 
 use App\DTO\LowestPriceEnquiry;
 use App\DTO\PromotionEnquiryInterface;
+use App\Entity\Promotion;
 use App\Filter\PromotionsFilterInterface;
+use App\Repository\ProductRepository;
 use App\Service\Serializer\DTOSerializer;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -14,6 +17,14 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ProductsController extends AbstractController
 {
+
+    public function __construct(
+        private ProductRepository $repository,
+        private EntityManagerInterface $entityManager
+    )
+    {
+    }
+
     #[Route('/products/{id}/lowest-price', name: 'lowest-price', methods: 'POST')]
     public function lowestPrice(Request $request, int $id, DTOSerializer $serializer, PromotionsFilterInterface $promotionsFilter): Response
     {
@@ -28,8 +39,15 @@ class ProductsController extends AbstractController
         /** @var LowestPriceEnquiry $lowestPriceEnquiry */
         $lowestPriceEnquiry = $serializer->deserialize($request->getContent(), LowestPriceEnquiry::class, 'json');
 
-        // Pass the Enquiry to appropriate Promotion Filter
-        $modifiedEnquiry = $promotionsFilter->apply($lowestPriceEnquiry);
+        $product = $this->repository->find($id);
+        $promotions = $this->entityManager->getRepository(Promotion::class)->findValidForProduct(
+            $product,
+            date_create_immutable($lowestPriceEnquiry->getRequestDate())
+        );
+
+        dd($promotions);
+
+        $modifiedEnquiry = $promotionsFilter->apply($lowestPriceEnquiry, $promotions);
 
         // Return Modified promotion
 
