86d0a9c0ec83d57157a087e1cf03e2d1f014e17d
Chavocito <fiifiowusu@aol.com>
2023-05-18 11:01:19 +0000

    add price filter

--- src/Filter/LowestPriceFilter.php
+++ src/Filter/LowestPriceFilter.php
@@ -4,16 +4,21 @@ namespace App\Filter;
 
 use App\DTO\LowestPriceEnquiry;
 use App\DTO\PromotionEnquiry;
+use App\Entity\Promotion;
 
 class LowestPriceFilter implements PromotionsFilterInterface
 {
 
-    public function apply(LowestPriceEnquiry $enquiry): LowestPriceEnquiry
+    public function apply(LowestPriceEnquiry $enquiry, Promotion|\App\Filter\Promotion ...$promotion): LowestPriceEnquiry
     {
+        $price = $enquiry->getPrice();
+        $quantity = $enquiry->getQuantity();
+        $lowestPrice = $quantity * $price;
+
         $enquiry->setDiscountedPrice(50);
         $enquiry->setPromotionId(3);
         $enquiry->setPrice(100);
-        $enquiry->setPromotionName('Alaha');
+        $enquiry->setPromotionName('BraQ Friday');
 
         return $enquiry;
     }
