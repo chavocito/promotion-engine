8cd51bdc4ba15a959ae96eca93928d4f976acedf
Chavocito <fiifiowusu@aol.com>
2023-06-05 03:01:59 +0000

    update subscriber test

--- tests/unit/DtoSubscriberTest.php
+++ tests/unit/DtoSubscriberTest.php
@@ -5,6 +5,8 @@ namespace App\Tests\unit;
 use App\DTO\LowestPriceEnquiry;
 use App\Event\AfterDtoCreatedEvent;
 use App\Tests\ServiceTestCase;
+use Symfony\Component\Validator\Exception\ValidationFailedException;
+use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
 
 class DtoSubscriberTest extends ServiceTestCase
 {
@@ -15,6 +17,14 @@ class DtoSubscriberTest extends ServiceTestCase
 
         $event = new AfterDtoCreatedEvent($dto);
 
-        $aDTO = $event->getDto();
+        /** @var EventDispatcherInterface $eventDispatcher */
+        $eventDispatcher = $this->container->get('debug.event_dispatcher');
+
+        //expect
+        $this->expectException(ValidationFailedException::class);
+        $this->expectExceptionMessage('This value should be positive.');
+
+        //when
+        $eventDispatcher->dispatch($event, $event::NAME);
     }
 }
